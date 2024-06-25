abc994953523b30fe518d0c373570ad8c3b155a8
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-25 20:43:00 +0200

    Attribute-Binding-and-Event-Handling

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,15 @@
 <template>
-  <div>
-    <p>
-      <input type="text" v-model="greeting" />
-    </p>
-    <p>{{ greeting }} {{ greeting.length }}</p>
-  </div>
+  
 </template>
 
 <script>
 export default {
   name: "App",
   data() {
-    return {
-      greeting: "Hello World",
-    };
+    
   },
   mounted() {
-    /* alert('hello') */
-    setTimeout(() => {
-      this.greeting = 'Changed';
-    }, 3000)
+   
   }
 };
 </script>
