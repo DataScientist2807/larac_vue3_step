de6a182d0be4c397fdbd86a40f1ae6cd6a24abb5
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-25 20:44:33 +0200

    Attribute-Binding-and-Event-Handling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  
+  <button>Click Me</button>
 </template>
 
 <script>
@@ -15,12 +15,11 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body {
+  height: 100%;
+}
+body {
+  display: grid;
+  place-items: center;
 }
 </style>
