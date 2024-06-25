e987f2032e16c56d7094f9b1aa7c7b518f7855f6
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-25 20:58:13 +0200

    Attribute-Binding-and-Event-Handling

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,45 @@
 <template>
-  <button>Click Me</button>
+  <div>
+    <!--  <button class="text-green">Click Me</button>
+    <button :class="buttonClasses">Click Me</button> -->
+    <!-- <button :class="buttonClasses" v-on:click="toggle">Click Me</button> -->
+    <button :class="active ? 'text-red' : 'text-green'" @click="toggle">Click Me</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
   data() {
-    
+    return {
+      /* buttonClasses: "text-green", */
+      active: false,
+    };
   },
-  mounted() {
-   
-  }
+  methods: {
+    toggle() {
+      /* alert('toggle'); */
+      /* this.buttonClasses = 'text-red' */
+      this.active = !this.active;
+    },
+  },
+  mounted() {},
 };
 </script>
 
 <style>
-html, body {
+html,
+body {
   height: 100%;
 }
 body {
   display: grid;
   place-items: center;
+}
+.text-red {
+  color: red;
+}
+.text-green {
+  color: green;
 }
 </style>
