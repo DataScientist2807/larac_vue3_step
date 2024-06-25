b42e0ce2a2a886fe1f7cd7016b70da7e4e5c0fcd
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-25 21:23:18 +0200

    Lists-Conditionals-and-Computed-Properties

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
-    <h2 class="font-bold mb-2">Assignments</h2>
-    <ul>
-      <li v-for="assignment in assignments" :key="assignment.id">
-        <label>{{ assignment.name }}
-          <input type="checkbox" v-model="assignment.complete">
-        </label>
-        
-      </li>
-    </ul>
-    <pre>
-      {{ assignments }}
-    </pre>
+    <section>
+      <h2 class="font-bold mb-2">In Progress</h2>
+      <ul>
+        <li v-for="assignment in assignments" :key="assignment.id">
+          <label
+            >{{ assignment.name }}
+            <input type="checkbox" v-model="assignment.complete" />
+          </label>
+        </li>
+      </ul>
+    </section>
+    <section class="mt-8">
+      <h2 class="font-bold mb-2 ">Completed</h2>
+      <ul>
+        <li v-for="assignment in assignments" :key="assignment.id">
+          <label
+            >{{ assignment.name }}
+            <input type="checkbox" v-model="assignment.complete" />
+          </label>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
@@ -21,10 +31,10 @@ export default {
   data() {
     return {
       assignments: [
-        { id: 0, name: 'Finish project', complete: false},
-        { id: 1, name: 'Read chapter 4', complete: false},
-        { id: 2, name: 'Turn in homework', complete: false}
-      ]
+        { id: 0, name: "Finish project", complete: false },
+        { id: 1, name: "Read chapter 4", complete: false },
+        { id: 2, name: "Turn in homework", complete: false },
+      ],
     };
   },
   methods: {
