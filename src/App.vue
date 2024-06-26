3e0f4c95a91f4bc5b2ff7230ab528386842ff93d
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-26 21:10:28 +0200

    Your-First-Custom-Vue-Component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  
+  <appbutton>Submitss</appbutton>
+  <appbutton>Click Me</appbutton>
 </template>
 
 <script>
+import appbutton from './components/AppsButton.vue'
+
 export default {
   name: "App",
+  components: appbutton,
   data() {
     return {
       
---
+++ src/components/AppsButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="bg-gray-600 hover:bg-gray-400 border rounded px-5 py-2" :disabled=cursor-not-allowed><slot /></button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      processing: false
+    };
+  },
+}
+</script>
+
+<style>
+
+</style>
